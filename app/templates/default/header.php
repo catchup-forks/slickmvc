358b717d42dbb52d526d3e5b2f9a7b01776a6e61
underdogg <nielsdrost7@gmail.com>
2015-11-01 18:19:02 +0100

    Added DataTables (jquery) and it needs a special class 'SPP', not working yet. Has its own MySQL PDO functions need to fix that

--- app/templates/default/header.php
+++ app/templates/default/header.php
@@ -14,24 +14,40 @@ $hooks = Hooks::get();
 <html lang="<?php echo LANGUAGE_CODE; ?>">
 <head>
 
-	<!-- Site meta -->
-	<meta charset="utf-8">
-	<?php
-	//hook for plugging in meta tags
-	$hooks->run('meta');
-	?>
-	<title><?php echo $data['title'].' - '.SITETITLE; //SITETITLE defined in app/Core/Config.php ?></title>
+  <!-- Site meta -->
+  <meta charset="utf-8">
+  <?php
+  //hook for plugging in meta tags
+  $hooks->run('meta');
+  ?>
+  <title><?php echo $data['title'] . ' - ' . SITETITLE; //SITETITLE defined in app/Core/Config.php ?></title>
 
-	<!-- CSS -->
-	<?php
-	Assets::css(array(
-		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
-		Url::templatePath() . 'css/style.css',
-	));
+  <!-- CSS -->
+  <?php
+  Assets::css(array(
+    '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+    Url::templatePath() . 'css/style.css',
+  ));
 
-	//hook for plugging in css
-	$hooks->run('css');
-	?>
+  Assets::js(array(
+    'https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.9,b-1.0.3,cr-1.2.0,fc-3.1.0,r-1.0.7/datatables.min.css',
+    'https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.9,b-1.0.3,cr-1.2.0,fc-3.1.0,r-1.0.7/datatables.min.js"',
+  ));
+
+  //hook for plugging in css
+  $hooks->run('css');
+  ?>
+
+  <script type="text/javascript" charset="utf-8">
+    $(document).ready(function() {
+      $('#example').dataTable( {
+        "bProcessing": true,
+        "bServerSide": true,
+        "sAjaxSource": "/processtickets"
+      } );
+    } );
+  </script>
+
 
 </head>
 <body>
--- app/views/tickets/browse.php
+++ app/views/tickets/browse.php
@@ -13,6 +13,28 @@ use MyApp\Core\Language;
 
 <p><?php echo $data['browse_message'] ?></p>
 
-<a class="btn btn-md btn-success" href="<?php echo DIR;?>">
-	<?php echo Language::show('back_home', 'Welcome'); ?>
-</a>
+<table cellpadding="0" cellspacing="0" border="0" class="display" id="example">
+  <thead>
+  <tr>
+    <th width="20%">Rendering engine</th>
+    <th width="25%">Browser</th>
+    <th width="25%">Platform(s)</th>
+    <th width="15%">Engine version</th>
+    <th width="15%">CSS grade</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr>
+    <td colspan="5" class="dataTables_empty">Loading data from server</td>
+  </tr>
+  </tbody>
+  <tfoot>
+  <tr>
+    <th>Rendering engine</th>
+    <th>Browser</th>
+    <th>Platform(s)</th>
+    <th>Engine version</th>
+    <th>CSS grade</th>
+  </tr>
+  </tfoot>
+</table>
